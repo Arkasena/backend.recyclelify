3b48774b04f6b83fa48057b6141aacda20183d08
mrkhairullah <mrkhairullah.id@gmail.com>
2024-06-05 15:33:16 +0700

    feat(db): add seeders according to the models

---
+++ prisma/seeders/data/transactionsAcceptanceRules.js
@@ -0,0 +1,118 @@
+const transactionsAcceptanceRules = [
+  {
+    partnerId: 2,
+    plasticId: 1,
+    pricePerKilogram: 2000,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 3,
+    plasticId: 2,
+    pricePerKilogram: 3000,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 4,
+    plasticId: 3,
+    pricePerKilogram: 2500,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 5,
+    plasticId: 4,
+    pricePerKilogram: 1500,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 6,
+    plasticId: 5,
+    pricePerKilogram: 2000,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 7,
+    plasticId: 6,
+    pricePerKilogram: 3000,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 8,
+    plasticId: 1,
+    pricePerKilogram: 2000,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 9,
+    plasticId: 2,
+    pricePerKilogram: 3000,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 10,
+    plasticId: 3,
+    pricePerKilogram: 2500,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 11,
+    plasticId: 4,
+    pricePerKilogram: 1500,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 12,
+    plasticId: 5,
+    pricePerKilogram: 2000,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 13,
+    plasticId: 6,
+    pricePerKilogram: 3000,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 14,
+    plasticId: 1,
+    pricePerKilogram: 2000,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 15,
+    plasticId: 2,
+    pricePerKilogram: 3000,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 16,
+    plasticId: 3,
+    pricePerKilogram: 2500,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 17,
+    plasticId: 4,
+    pricePerKilogram: 1500,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 18,
+    plasticId: 5,
+    pricePerKilogram: 2000,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 19,
+    plasticId: 6,
+    pricePerKilogram: 3000,
+    minimumTransactionWeight: 10,
+  },
+  {
+    partnerId: 20,
+    plasticId: 1,
+    pricePerKilogram: 2000,
+    minimumTransactionWeight: 10,
+  },
+];
+
+module.exports = transactionsAcceptanceRules;
