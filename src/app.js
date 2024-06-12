1efa6301a83bbdf6cfcd592aab368595207c685f
mrkhairullah <mrkhairullah.id@gmail.com>
2024-06-12 17:12:36 +0700

    fix: restore the previous structure

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const prismaClient = require("./utilities/prismaClient.utility");
 const api = require("./routes/api.route");
 
 const app = express();
@@ -13,30 +12,6 @@ app.use("/api", api);
 
 app.get("/", async (req, res) => {
   res.send("♻️ Recyclelify");
-});
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, async () => {
-  console.log("🚀 Server running on port", port);
-});
-
-process.on("unhandledRejection", async (reason, promise) => {
-  console.group("Unhandled Rejection");
-  console.error("reason:", reason);
-  console.error("at:", promise);
-  console.groupEnd();
-  await prismaClient.$disconnect();
-  process.exit(1);
-});
-
-process.on("uncaughtException", async (error, origin) => {
-  console.group("Uncaught Exception");
-  console.error("origin:", origin);
-  console.error("error:", error);
-  console.groupEnd();
-  await prismaClient.$disconnect();
-  process.exit(1);
 });
 
 module.exports = app;
