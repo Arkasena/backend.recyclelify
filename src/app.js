4c5b92f07d326f6314b0e91ef542a6b3a36eaf03
mrkhairullah <mrkhairullah.id@gmail.com>
2024-06-12 14:03:52 +0700

    feat(routes): register api route on the app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const api = require("./routes/api.route");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", api);
 
 app.get("/", async (req, res) => {
   res.send("♻️ Recyclelify");
