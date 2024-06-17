2a6da62be86099f22b5444ac3b5447c4105484e4
mrkhairullah <mrkhairullah.id@gmail.com>
2024-06-17 20:14:41 +0700

    refactor(api): adjustments method and middlewares

--- src/utilities/resource.utility.js
+++ src/utilities/resource.utility.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
-function resource(controller) {
+function resource(controller, middlewares = [], strict = false) {
   const router = express.Router();
 
-  router.get(`/`, controller.index);
-  router.get(`/:id`, controller.show);
-  router.post(`/`, controller.save);
-  router.put(`/:id`, controller.update);
-  router.delete(`/:id`, controller.delete);
+  router.get(`/`, strict ? middlewares : [], controller.index);
+  router.get(`/:id`, strict ? middlewares : [], controller.show);
+  router.post(`/`, middlewares, controller.save);
+  router.put(`/:id`, middlewares, controller.update);
+  router.delete(`/:id`, middlewares, controller.delete);
 
   return router;
 }
